d99bc393a6fa5d23ac7a7a60fd184eea5ad580d7
xw390668 <xw390668@alibaba-inc.com>
2023-02-06 17:33:33 +0800

    fix:basic

--- webpack.config.js
+++ webpack.config.js
@@ -1,105 +1,118 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// 引入一个包
+const path = require('path');
+// 引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+// 引入clean插件
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-//webpack中的所有的配置信息都应该写在module.exports中
-
+// webpack中的所有的配置信息都应该写在module.exports中
 module.exports = {
-    optimization: {
-        minimize: false // 关闭代码压缩，可选
-    },
+    // 指定入口文件
+    entry: "./src/index.ts",
 
-    entry: "./src/index.ts",//指定入口文件
+    mode:'development',
 
-    mode: 'development',
-
-    devtool: "inline-source-map",
-
-    devServer: {
-        contentBase: './dist'
-    },
-
-    //指定打包文件所在目录
+    // 指定打包文件所在目录
     output: {
-        //指定打包文件的目录
-        path: path.resolve(__dirname, "dist"),
-        //打包后文件名
+        // 指定打包文件的目录
+        path: path.resolve(__dirname, 'dist'),
+        // 打包后文件的文件
         filename: "bundle.js",
+
+        // 告诉webpack不使用箭头
         environment: {
-            arrowFunction: false // 关闭webpack的箭头函数，可选
+            arrowFunction: false,
+            // 不使用const,此时兼容IE 10
+            const: false
         }
     },
 
-    resolve: {
-        extensions: [".ts", ".js"]
-    },
-
-    //指定webpack打包时要使用的模块
+    // 指定webpack打包时要使用模块
     module: {
-        //指定要加载的规则
+        // 指定要加载的规则
         rules: [
             {
-                //指定规则生效的文件【正则表达式】
+                // test指定的是规则生效的文件
                 test: /\.ts$/,
-                //要使用的loader
+                // 要使用的loader
                 use: [
+                    // 配置babel
                     {
+                        // 指定加载器
                         loader: "babel-loader",
+                        // 设置babel
                         options: {
+                            // 设置预定义的环境
                             presets: [
                                 [
-                                    //指定环境的插件
+                                    // 指定环境的插件
                                     "@babel/preset-env",
-                                    //配置信息
+                                    // 配置信息
                                     {
-                                        //要兼容的目标浏览器
-                                        "targets": {
+                                        // 要兼容的目标浏览器
+                                        targets: {
                                             "chrome": "58",
                                             "ie": "11"
                                         },
-                                        //指定corejs的版本
+                                        // 指定corejs的版本
                                         "corejs": "3",
-                                        //使用corejs的方式，usage表示按需加载
+                                        // 使用corejs的方式 "usage" 表示按需加载
                                         "useBuiltIns": "usage"
                                     }
                                 ]
                             ]
                         }
                     },
-                    {
-                        loader: "ts-loader"
-                    }
+                    'ts-loader'
                 ],
-                //要排除的文件
-                exclude: /node_modules/
+                // 要排除的文件
+                exclude: /node-modules/
             },
+
+            // 设置less文件的处理
             {
                 test: /\.less$/,
                 use: [
                     "style-loader",
                     "css-loader",
+
+                    // 引入postcss
+                    // 类似于babel，把css语法转换兼容旧版浏览器的语法
                     {
                         loader: "postcss-loader",
                         options: {
                             postcssOptions: {
                                 plugins: [
-                                    "postcss-preset-env",
-                                    {
-                                        browsers: 'last 2 versions'
-                                    }
+                                    [
+                                        // 浏览器兼容插件
+                                        "postcss-preset-env",
+                                        {
+                                            // 每个浏览器最新两个版本
+                                            browsers: 'last 2 versions'
+                                        }
+                                    ]
                                 ]
                             }
                         }
-                    }
+                    },
+                    "less-loader"
                 ]
             }
         ]
     },
 
+    // 配置Webpack插件
     plugins: [
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'TS测试'
+        new HTMLWebpackPlugin({
+            // title: "这是一个自定义的title"
+            template: "./src/index.html"
         }),
-    ]
-}
+    ],
+
+    // 用来设置引用模块
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+
+};
